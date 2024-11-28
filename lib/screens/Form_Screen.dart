13d54be300fae3a396324408e1a93135d1ec0e74
Theo Pinheiro <19900@bemol.local>
2024-11-28 09:29:13 -0400

    feat: adcionado validação para o formulario

--- lib/screens/Form_Screen.dart
+++ lib/screens/Form_Screen.dart
@@ -11,119 +11,130 @@ class _FormScreenState extends State<FormScreen> {
   TextEditingController characterNameController = TextEditingController();
   TextEditingController characterSpeedController = TextEditingController();
   TextEditingController characterUrlController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Adcionar Novo Personagem',
-          style: TextStyle(color: Colors.white),
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Adcionar Novo Personagem',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blue,
         ),
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            height: 650,
-            width: 375,
-            decoration: BoxDecoration(
-                color: Colors.black12,
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(width: 3, color: Colors.lightBlue)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    validator: (String? value) {
-                      if (value != null && value.isEmpty) {
-                        return 'Insira um nome para o personagem';
-                      }
-                      return null;
-                    },
-                    controller: characterNameController,
-                    textAlign: TextAlign.center,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Nome',
-                      fillColor: Colors.white70,
-                      filled: true,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    controller: characterSpeedController,
-                    textAlign: TextAlign.center,
-                    validator: (value) {
-                      if (value!.isEmpty ||
-                          int.parse(value) > 5 ||
-                          int.parse(value) < 1) {
-                        return 'Insira a velocidade entre 1 e 5';
-                      }
-                    },
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Velocidade',
-                      fillColor: Colors.white70,
-                      filled: true,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Por favor insira uma URL válida';
-                      }
-                      return null;
-                    },
-                    controller: characterUrlController,
-                    textAlign: TextAlign.center,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Foto (URL)',
-                      fillColor: Colors.white70,
-                      filled: true,
-                    ),
-                  ),
-                ),
-                Container(
-                  height: 100,
-                  width: 72,
-                  decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(width: 2, color: Colors.blue)),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.network(
-                      characterUrlController.text,
-                      errorBuilder: (BuildContext context, Object exception,
-                          StackTrace? stackTrace) {
-                        return Image.asset(
-                          'assets/images/empty_image.png',
-                        );
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              height: 650,
+              width: 375,
+              decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(width: 3, color: Colors.lightBlue)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (String? value) {
+                        if (value != null && value.isEmpty) {
+                          return 'Insira um nome para o personagem';
+                        }
+                        return null;
                       },
-                      fit: BoxFit.cover,
+                      controller: characterNameController,
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Nome',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
                     ),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: const ButtonStyle(
-                    foregroundColor: WidgetStatePropertyAll(Colors.white),
-                    backgroundColor: WidgetStatePropertyAll(Colors.lightBlue),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      textAlign: TextAlign.center,
+                      validator: (value) {
+                        if (value!.isEmpty ||
+                            int.parse(value) > 5 ||
+                            int.parse(value) < 1) {
+                          return 'Insira a velocidade entre 1 e 5';
+                        }
+                      },
+                      controller: characterSpeedController,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Velocidade',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
                   ),
-                  child: const Text(
-                    'Adcionar!',
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Por favor insira uma URL válida';
+                        }
+                        return null;
+                      },
+                      controller: characterUrlController,
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Foto (URL)',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                  Container(
+                    height: 100,
+                    width: 72,
+                    decoration: BoxDecoration(
+                        color: Colors.lightBlue,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(width: 2, color: Colors.lightBlue)),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        characterUrlController.text,
+                        errorBuilder: (BuildContext context, Object exception,
+                            StackTrace? stackTrace) {
+                          return Image.asset(
+                            'assets/images/empty_image.png',
+                          );
+                        },
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        print(this.characterNameController.text);
+                        print(this.characterUrlController.text);
+                        print(this.characterSpeedController.text);
+                      }
+                    },
+                    style: const ButtonStyle(
+                      foregroundColor: WidgetStatePropertyAll(Colors.white),
+                      backgroundColor: WidgetStatePropertyAll(Colors.lightBlue),
+                    ),
+                    child: const Text(
+                      'Adcionar!',
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
